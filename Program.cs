af542eee25e5e8e45482ccca81df3b9b7ce95159
timmi <Tima1>
2022-01-19 17:09:45 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name},сегодня {DateTime.Now:dd.MM.yyyy}");
+            Console.ReadLine();
+        }
+    }
+}
